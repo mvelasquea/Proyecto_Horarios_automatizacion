86f7bc246b4cc10542deb40d971088f44848e71d
sxrv <sriveros@unsa.edu.pe>
2025-07-08 17:09:02 -0500

    carga de datos al grafo funcional

--- include/Sistema.hpp
+++ include/Sistema.hpp
@@ -15,25 +15,18 @@ private:
     std::unordered_map<std::string, Curso> cursos; // curso_codigo -> Curso
     Grafo<Grupo*> grafoGrupos; 
     std::vector<Horario> horarios;
-    std::vector<Grupo*> gruposParaBacktracking;
 
     void backtrackingHorarios(std::vector<Grupo*>& seleccionados, size_t index);
 public:
     Sistema();
     //generacion de horarios
-    void backtrackingHorarios(vector<Grupo*>& seleccionados, size_t index,
-                                 unordered_map<string, bool>& cursoConTeorico,
-                                 unordered_map<string, bool>& cursoConLab);
-    void prepararGruposParaBacktracking();
     const std::vector<Horario>& getHorarios() const { return horarios; }
-    void generarHorarios(bool incluirLaboratorios);
 
     // Carga cursos desde CSV
     void cargarCursos(const std::string& rutaCSV);
     void cargarGrupos(const std::string& rutaCSV);
     Hora parsearHora(const std::string& strHora, const std::string& aula);
     void cargarGrafo();
-    void verificarDatosCargados() const;
 
     // Acceso
     Curso* obtenerCurso(const std::string& codigo);
